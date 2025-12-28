1b5158912be958ac31bfa392713e94f273130670
GersonESS <gebhsantos@gmail.com>
2025-12-28 15:37:44 -0300

    Update db.js

---
+++ 00r/database/db.js
@@ -0,0 +1,13 @@
+const  moongoose = require('mongoose');
+
+const connectToDb =  () => 
+    {   
+        moongoose
+        .connect(process.env.DB_URI)
+        .then(() => {
+            console.log('Connectou com sucesso to MongoDB');
+        }).catch((err) => {
+            console.error('Error connecting to MongoDB:', err);
+        });
+    }
+module.exports = connectToDb; 
