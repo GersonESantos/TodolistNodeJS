377c3a903c3f036aa526f36e2027eab8cd0f8cad
GersonESS <gebhsantos@gmail.com>
2025-12-29 10:43:47 -0300

    g

--- 00r/index.js
+++ 00r/index.js
@@ -3,7 +3,8 @@ const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
-
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
   res.render('index')
 })
