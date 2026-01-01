1157592191a32be1ed51812eb42464d0592a6f1c
GersonESS <gebhsantos@gmail.com>
2026-01-01 19:23:08 -0300

    a

---
+++ conect/database/db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const connectToDb = () => {
+  mongoose
+    .connect(process.env.DB_URI)
+    .then(() => console.log("MongoDB Atlas CONECTADO!"))
+    .catch((err) => console.log(err));
+};
+
+module.exports = connectToDb;
