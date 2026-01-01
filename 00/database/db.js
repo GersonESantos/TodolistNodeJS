ed2e3cd8f7b05e4817d283a025980d85e6e2f475
GersonESS <gebhsantos@gmail.com>
2026-01-01 19:20:39 -0300

    Update db.js

--- 00/database/db.js
+++ 00/database/db.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDb = () => {
   mongoose
-    .connect(process.env.DB_URI, 
-      {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(process.env.DB_URI)
     .then(() => console.log("MongoDB Atlas CONECTADO!"))
     .catch((err) => console.log(err));
 };
