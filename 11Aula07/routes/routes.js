5eaf545ac62830f9b1c33b4e8f846f6a890d2672
GersonESS <gebhsantos@gmail.com>
2026-01-03 17:36:27 -0300

    a

---
+++ 11Aula07/routes/routes.js
@@ -0,0 +1,10 @@
+const routes = require('express').Router();
+const TaskController = require('../controller/TaskController');
+
+routes.get('/', TaskController.getAllTasks);
+routes.post('/create', TaskController.createTask);
+routes.get('/getById/:id/:method', TaskController.getById);
+routes.post('/updateOneTask/:id', TaskController.updateOneTask);
+routes.get('/deleteTask/:id', TaskController.deleteOneTask);
+routes.get('/Check/:id', TaskController.taskCheck);
+module.exports = routes;
